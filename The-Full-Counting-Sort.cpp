57eba82c9067ada1d841d3b2ef9ca5b51e37f0b4
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-12-18 02:07:57 +0900

    Update The-Full-Counting-Sort.cpp

--- The-Full-Counting-Sort.cpp
+++ The-Full-Counting-Sort.cpp
@@ -1,1 +1,19 @@
+void countSort(vector<vector<string>> arr)
+{
+    std::multimap<int, string> mm;
 
+    for (int i = 0 ; i < arr.size()/2 ; ++i)
+    {
+        arr[i][1] = "-";
+    }
+
+    for (const auto& s : arr)
+    {
+        mm.insert(make_pair(stoi(s[0]), s[1]));
+    }
+
+    for (const auto& m : mm)
+    {
+        std::cout << m.second + " ";
+    }
+}
