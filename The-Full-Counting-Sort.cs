6fefe899ac36f03d960c57c2c54b5a04924ccecb
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-08-16 05:29:28 +0900

    Update The-Full-Counting-Sort.cs

--- The-Full-Counting-Sort.cs
+++ The-Full-Counting-Sort.cs
@@ -1,1 +1,32 @@
-The-Full-Counting-Sort
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<string>> arr = new List<List<string>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList());
+        }
+
+        Result.countSort(arr);
+    }
+}
