f369961074efeb2b8ee1c7cec54049d37f31e9e6
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-08-19 04:41:05 +0900

    Update The-Full-Counting-Sort.java

--- The-Full-Counting-Sort.java
+++ The-Full-Counting-Sort.java
@@ -1,1 +1,26 @@
-The-Full-Counting-Sort
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<String>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        Result.countSort(arr);
+
+        bufferedReader.close();
+    }
+}
