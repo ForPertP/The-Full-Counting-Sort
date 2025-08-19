4102e8fc2488ca531a1ef01266efdba47bd3316a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-08-20 06:20:35 +0900

    Update The-Full-Counting-Sort.java

--- The-Full-Counting-Sort.java
+++ The-Full-Counting-Sort.java
@@ -19,7 +19,33 @@ class Result {
      */
 
     public static void countSort(List<List<String>> arr) {
-    }        
+        
+        int n = arr.size();
+        List<List<String>> buckets = new ArrayList<>(100);
+        for (int i = 0; i < 100; i++) {
+            buckets.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+            int key = Integer.parseInt(arr.get(i).get(0));
+            String value = (i < n / 2) ? "-" : arr.get(i).get(1);
+            buckets.get(key).add(value);
+        }
+
+        
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            if (buckets.get(i) != null) {
+                for (String str : buckets.get(i)) {
+                    result.append(str).append(" ");
+                }
+            }
+        }
+        
+        System.out.print(result.toString().trim());
+    }
+
+
 }
 
 
